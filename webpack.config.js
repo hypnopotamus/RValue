dcbc90634e77fa4f977d31b5de3037393d5238e6
christopher dykstra <chrdyks@gmail.com>
2023-01-28 08:49:53 -0600

    empty react app

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
+        },
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            scriptLoading: "blocking"
+        }),
+    ],
+};
